1fd1aace8a3b89247be3f7db9b0b299b70191bd9
kinshuk <kinshuk18239@iiitd.ac.in>
2020-04-16 00:32:17 +0530

    added

---
+++ dbms.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,redirect,url_for,request
+from flask_bootstrap import Bootstrap
+import os
+template_dir = os.getcwd()
+
+app=Flask(__name__,static_folder='static')
+
+Bootstrap(app)
+@app.route('/',methods=['GET','POST'])
+def index():
+	return render_template('index.html')
+
+@app.route('/handle_data',methods=['GET'])
+def handle_data():
+	if 'client' in str(request):
+		print("client selected")
+		return redirect(url_for('LoginClient'))
+
+	
+	return redirect(url_for('index'))
+
+	print(request)
+@app.route('/client',methods=['GET','POST'])
+def LoginClient():
+	return ("Client")
+	# elif request.form['submit'] == 'judges':
+	# 	return redirect(url_for('index'))
+	return 
+if __name__ == '__main__':
+	app.run(debug=True)
