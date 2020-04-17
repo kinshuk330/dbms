9ed634e64b56720c61aea506c9fbe3ab2f5dd444
Dushyant Panchal <panchal.d1999@gmail.com>
2020-04-17 12:32:34 +0530

    downloaded templates

--- dbms.py
+++ dbms.py
@@ -3,41 +3,15 @@ from flask_bootstrap import Bootstrap
 import os
 template_dir = os.getcwd()
 
-app=Flask(__name__,static_folder='static')
+app=Flask(__name__)
 
 Bootstrap(app)
 
-@app.route('/',methods=['GET','POST'])
-def index():
+
+@app.route('/')
+def home():
 	return render_template('index.html')
 
 
-@app.route('/Signupas',methods=['GET','POST'])
-
-def Signupas():
-	return render_template('Signupas.html')
-
-
-@app.route('/Signup',methods=['GET'])
-
-def Signup():
-	if 'client' in str(request):
-		message="Client"
-	if 'judge' in str(request):
-		message="Judge"
-	if 'lawyer' in str(request):
-		message="Lawyer"
-	if 'firm' in str(request):
-		message="Firm"
-	return render_template('Signup.html',message=message)
-
-
-
-@app.route('/Login',methods=['GET','POST'])
-def Login():
-	return (str(request))
-	# elif request.form['submit'] == 'judges':
-	# 	return redirect(url_for('index'))
-	return 
 if __name__ == '__main__':
 	app.run(debug=True)
