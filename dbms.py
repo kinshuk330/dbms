cd14f0b35d70cd8a79a939955f86aa50d126021b
kinshuk <kinshuk18239@iiitd.ac.in>
2020-04-17 23:38:18 +0530

    login forms completed as well as data parsing

--- dbms.py
+++ dbms.py
@@ -1,26 +1,29 @@
 from flask import Flask,render_template,redirect,url_for,request
 from flask_bootstrap import Bootstrap
 import os
-template_dir = os.getcwd()
 
 app=Flask(__name__,static_folder='static')
 
 Bootstrap(app)
-
 @app.route('/',methods=['GET','POST'])
-def index():
+def Login():
+	if request.method=='POST':
+		username=request.form.get('username')
+		password=request.form.get('password')
+		print(username+" "+str(password))
+		if username =='k' and password !='kinshuk':
+			message='wrongpass'
+			redirect(url_for('Login'))
+			return render_template('index.html',message=message)
 	return render_template('index.html')
 
 
 @app.route('/Signupas',methods=['GET','POST'])
 
 def Signupas():
-	return render_template('Signupas.html')
 
-
-@app.route('/Signup',methods=['GET'])
-
-def Signup():
+	message=None
+	
 	if 'client' in str(request):
 		message="Client"
 	if 'judge' in str(request):
@@ -29,15 +32,58 @@ def Signup():
 		message="Lawyer"
 	if 'firm' in str(request):
 		message="Firm"
+	print(str(message))
+	if message!=None:
+		return redirect(url_for('Signup',message=message))
+	return render_template('Signupas.html')
+
+
+@app.route('/Signup/<message>',methods=['GET','POST'])
+
+def Signup(message):
+	if request.method=='GET':
+		pass
+	else:
+
+		username=request.form.get('username')
+		password=request.form.get('password')
+		message=request.form.get('message')
+
+		if message=='Firm':
+			firmname=request.form.get('firmname')
+			est=request.form.get('est')
+			areaspe=request.form.get('areaspe')
+			ls=request.form.get('ls')
+		else:
+			firstname=request.form.get('firstname')
+			lastname=request.form.get('lastname')
+			if message=='Client':
+				dob=request.form.get('dob')
+			elif message=='Lawyer':
+				ed=request.form.get('ed')
+				specarea=request.form.get('specarea')
+				AIBE=request.form.get('AIBE')
+				lis=request.form.get('lis')
+			else:
+				src=request.form.get('src')	
+				doa=request.form.get('doa')
+			print(firstname +" "+lastname+" "+password+" "+username+" "+dob+" "+message )
+		if username =='k':
+			message1='enter again'
+			redirect(url_for('Signup'))
+			return render_template('Signup.html',message=message,message1=message1)
+		else:
+			return redirect(url_for('Home'))
+
 	return render_template('Signup.html',message=message)
 
 
 
-@app.route('/Login',methods=['GET','POST'])
-def Login():
-	return (str(request))
+@app.route('/Home',methods=['GET','POST'])
+def Home():
+	return ('login')
 	# elif request.form['submit'] == 'judges':
 	# 	return redirect(url_for('index'))
-	return 
+	 
 if __name__ == '__main__':
 	app.run(debug=True)
