2afa5d2b3a171f9d97189200e07254162a816f93
Navneet Agarwal <53942457+navneet-ag@users.noreply.github.com>
2020-10-01 15:04:22 +0530

    Update dbms.py

--- dbms.py
+++ dbms.py
@@ -1,10 +1,13 @@
+# Importing the libaries 
 from flask import Flask,render_template,redirect,url_for,request
 from flask_bootstrap import Bootstrap
 import os
+# get the current working directory
 template_dir = os.getcwd()
 
 app=Flask(__name__,static_folder='static')
 
+# Definding the functions
 Bootstrap(app)
 @app.route('/',methods=['GET','POST'])
 def index():
@@ -26,5 +29,6 @@ def LoginClient():
 	# elif request.form['submit'] == 'judges':
 	# 	return redirect(url_for('index'))
 	return 
+# The main function
 if __name__ == '__main__':
 	app.run(debug=True)
